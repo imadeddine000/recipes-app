85867d0944a9e0b5f1a750e48d22655d33478eec
imadeddine kebour <46073342+imadeddine000@users.noreply.github.com>
2023-04-19 23:40:51 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,19 +6,6 @@ import { collection,getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyCZIn0LlYFWZ1hBNii_J734DhJ7B6cjjOw",
-  
-    authDomain: "ollyfood-d5f9d.firebaseapp.com",
-  
-    projectId: "ollyfood-d5f9d",
-  
-    storageBucket: "ollyfood-d5f9d.appspot.com",
-  
-    messagingSenderId: "357700993827",
-  
-    appId: "1:357700993827:web:93df56261f9268520f8296",
-  
-    measurementId: "G-W3PXD1SJ0T"
   
   };
   
